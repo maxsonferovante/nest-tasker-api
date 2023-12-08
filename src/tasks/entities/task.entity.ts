93711d6fa5d5378a81468dbb0d2dd9b1dfa138cc
Maxson Ferovante <maxsonferovante@users.noreply.github.com>
2023-12-08 17:24:29 -0300

    create entity task

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,15 @@
-export class Task {}
+import { randomUUID } from "crypto";
+
+export class Task {
+    private id: string;
+    private title: string;
+    private description: string;
+    private done: boolean;
+
+    constructor({ title, description, done }: { title: string; description: string; done: boolean; }) {
+        this.id = randomUUID().toString();
+        this.title = title || 'task_' + this.id;
+        this.description = description || '';
+        this.done = done || false;
+    }
+}
