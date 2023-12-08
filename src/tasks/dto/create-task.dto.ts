f0a284b26afd7624a5b748c5ffcf473058015037
Maxson Ferovante <maxsonferovante@users.noreply.github.com>
2023-12-08 16:52:38 -0300

    config class validation in create-task

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,16 @@
+import { IsBoolean, IsEmpty, IsOptional, IsString } from 'class-validator';
+
+
 export class CreateTaskDto {
+    @IsEmpty()
+    @IsString()
     title: string;
+
+    @IsOptional()
+    @IsString()
     description: string;
+
+    @IsOptional()
+    @IsBoolean()
     done: boolean;
 }
