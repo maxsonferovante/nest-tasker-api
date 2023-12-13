32b114ad4e83c6a9282bceede2c9ccad851dc9c0
Maxson Ferovante <maxsonferovante@users.noreply.github.com>
2023-12-12 22:16:33 -0300

    create service prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [TasksModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
