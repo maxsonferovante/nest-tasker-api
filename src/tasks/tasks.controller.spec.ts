99b0e84fa5f81bbf5491e7e3353f5d759f8ab955
Maxson Ferovante <maxsonferovante@users.noreply.github.com>
2023-12-12 22:38:21 -0300

    add in providers of test PrismaService

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('TasksController', () => {
   let controller: TasksController;
@@ -8,7 +9,7 @@ describe('TasksController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TasksController],
-      providers: [TasksService],
+      providers: [TasksService, PrismaService],
     }).compile();
 
     controller = module.get<TasksController>(TasksController);
